f9b9e41151465ab45ffa26bdf491c45e00e90254
SifaKasena <sifa.collins@gmail.com>
2023-03-07 11:44:43 +0300

    [Fix:] Typo and no of lines

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-msg = 'and that piece of art is useful - Dora Korpar, 2015-10-191789\n'
+msg = 'and that piece of art is useful - Dora Korpar, 2015-10-19\n'
 sys.stderr.write(msg)
 quit(1)
