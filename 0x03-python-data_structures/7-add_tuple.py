456cb2ea7cb4d52d98ae79235281e7cbce5dcad8
SifaKasena <sifa.collins@gmail.com>
2023-03-11 15:39:34 +0300

    Attempt remaining mandatory tasks

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    while len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,)
+    while len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
+    a, b = tuple_a;
+    c, d = tuple_b;
+    return (a + c, b + d)
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first = None
+    return(length, first)
