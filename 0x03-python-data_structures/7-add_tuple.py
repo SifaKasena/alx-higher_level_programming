37536752528393b72b0e05d4a619bd81fc09f931
SifaKasena <sifa.collins@gmail.com>
2023-03-13 12:36:45 +0300

    Fix pycodestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0,)
     while len(tuple_b) < 2:
         tuple_b = tuple_b + (0,)
-    a, b = tuple_a;
-    c, d = tuple_b;
+    a, b = tuple_a[:2]
+    c, d = tuple_b[:2]
     return (a + c, b + d)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         first = None
-    return(length, first)
+    else:
+        first = sentence[0]
+    return (length, first)
